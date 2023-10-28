8ad6cd17926637bb097787d2cbaef7f434778fe5
sanosuke216 <gretsch696@gmail.com>
2023-10-29 02:17:34 +0900

    3sensorとモーター対応

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -18,29 +18,35 @@ AsyncWebServer server(80);
 
 #define DHT1 27
 #define DHT2 19
-//#define DHT3 25
+#define DHT3 25
+#define FAN 26
 // 25, 27, 32
 
 #define DHTTYPE DHT22 // DHT 22 (AM2302)
 
-DHT_Unified dht1(DHT1, DHTTYPE);
-DHT_Unified dht2(DHT2, DHTTYPE);
-//DHT_Unified dht3(DHT3, DHTTYPE);
+DHT_Unified ShoesSensor1(DHT1, DHTTYPE);
+DHT_Unified ShoesSensor2(DHT2, DHTTYPE);
+DHT_Unified RoomSensor(DHT3, DHTTYPE);
 
 uint32_t delayMS;
 
-float temperature1, temperature2, temperature3;
-float humidity1, humidity2, humidity3;
+float ShoesTemp1, ShoesTemp2, RoomTemp;
+float ShoesHumi1, ShoesHumi2, RoomHumi;
+
+int count = 0;
 
 void setup() {
   M5.begin();
   //Serial.begin(115200);
   // Initialize device.
-  dht1.begin();
-  dht2.begin();
-  //dht3.begin();
+  ShoesSensor1.begin();
+  ShoesSensor2.begin();
+  RoomSensor.begin();
   sensor_t sensor;
   delayMS = sensor.min_delay;
+  pinMode(FAN, OUTPUT);
+
+  digitalWrite(FAN,1);
 
   // WiFi Setup
 
@@ -68,16 +74,16 @@ void setup() {
     String jsonData = "";
     jsonData += "{\n";
     jsonData += "  \"sensor1\": {\n";
-    jsonData += "    \"temperature\": " + String(temperature1, 2) + ",\n";
-    jsonData += "    \"humidity\": " + String(humidity1, 2) + "\n";
+    jsonData += "    \"temperature\": " + String(ShoesTemp1, 2) + ",\n";
+    jsonData += "    \"humidity\": " + String(ShoesHumi1, 2) + "\n";
     jsonData += "  },\n";
     jsonData += "  \"sensor2\": {\n";
-    jsonData += "    \"temperature\": " + String(temperature2, 2) + ",\n";
-    jsonData += "    \"humidity\": " + String(humidity2, 2) + "\n";
+    jsonData += "    \"temperature\": " + String(ShoesTemp2, 2) + ",\n";
+    jsonData += "    \"humidity\": " + String(ShoesHumi2, 2) + "\n";
     jsonData += "  },\n";
     jsonData += "  \"sensor3\": {\n";
-    jsonData += "    \"temperature\": " + String(temperature3, 2) + ",\n";
-    jsonData += "    \"humidity\": " + String(humidity3, 2) + "\n";
+    jsonData += "    \"temperature\": " + String(RoomTemp, 2) + ",\n";
+    jsonData += "    \"humidity\": " + String(RoomHumi, 2) + "\n";
     jsonData += "  }\n";
     jsonData += "}\n";
     request->send(200, "application/json", jsonData);
@@ -93,63 +99,71 @@ void loop() {
   delay(delayMS);
   // Get temperature event and print its value.
   sensors_event_t event;
-  // DHT1
+  // ShoesSensor1
 
-  dht1.temperature().getEvent(&event);
+  ShoesSensor1.temperature().getEvent(&event);
   if (isnan(event.temperature)) {
     //Serial.println(F("Error reading temperature!"));
-    M5.Lcd.printf("Error reading temperature1!");
+    M5.Lcd.printf("Error reading ShoesTemp1!");
   } else {
-    temperature1 = event.temperature;
+    ShoesTemp1 = event.temperature;
   }
   // Get humidity event and print its value.
-  dht1.humidity().getEvent(&event);
+  ShoesSensor1.humidity().getEvent(&event);
   if (isnan(event.relative_humidity)) {
     //Serial.println(F("Error reading humidity!"));
-    M5.Lcd.printf("Error reading humidity1!");
+    M5.Lcd.printf("Error reading ShoesHumi1!");
   } else {
-    humidity1 = event.relative_humidity;
+    ShoesHumi1 = event.relative_humidity;
   }
 
-  // DHT2
-  dht2.temperature().getEvent(&event);
+  // ShoesSensor2
+  ShoesSensor2.temperature().getEvent(&event);
   if (isnan(event.temperature)) {
     //Serial.println(F("Error reading temperature!"));
-    M5.Lcd.printf("Error reading temperature2!");
+    M5.Lcd.printf("Error reading ShoesTemp2!");
   } else {
-    temperature2 = event.temperature;
+    ShoesTemp2 = event.temperature;
   }
   // Get humidity event and print its value.
-  dht2.humidity().getEvent(&event);
+  ShoesSensor2.humidity().getEvent(&event);
   if (isnan(event.relative_humidity)) {
     //Serial.println(F("Error reading humidity!"));
-    M5.Lcd.printf("Error reading humidity2!");   
+    M5.Lcd.printf("Error reading ShoesHumi2!");   
   } else {
-    humidity2 = event.relative_humidity;
+    ShoesHumi2 = event.relative_humidity;
   }
-#if 0
-  // DHT3
-  dht3.temperature().getEvent(&event);
+
+  // RoomSensor
+  RoomSensor.temperature().getEvent(&event);
   if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
+    //Serial.println(F("Error reading temperature!"));
+    M5.Lcd.printf("Error reading RoomTemp!");   
   } else {
-    temperature3 = event.temperature;
+    RoomTemp = event.temperature;
   }
   // Get humidity event and print its value.
-  dht3.humidity().getEvent(&event);
+  RoomSensor.humidity().getEvent(&event);
   if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
+    //Serial.println(F("Error reading humidity!"));
+    M5.Lcd.printf("Error reading RoomHumi!");    
   } else {
-    humidity3 = event.relative_humidity;
+    RoomHumi = event.relative_humidity;
   }
-#endif 
-  //Serial.printf("Temperature: %f *C \t Humidity: %f %% \n", temperature1,
-                //humidity1);
+  //Serial.printf("Temperature: %f *C \t Humidity: %f %% \n", ShoesTemp1,
+                //ShoesHumi1);
   M5.Lcd.fillScreen(BLACK);  // 画面の塗りつぶし
   M5.Lcd.setCursor(0, 0);  // 文字列の書き出し位置
   M5.Lcd.setTextSize(3);  // 文字サイズを設定  
-  M5.Lcd.printf("Temperature1: %f *C \t Humidity1: %f %% \n", temperature1,
-                humidity1);
-  M5.Lcd.printf("Temperature2: %f *C \t Humidity2: %f %% \n", temperature2,
-                humidity2);                         
+  M5.Lcd.printf("ShoesTemp1: %f *C \t ShoesHumi1: %f %% \n", ShoesTemp1,
+                ShoesHumi1);
+  M5.Lcd.printf("ShoesTemp2: %f *C \t ShoesHumi2: %f %% \n", ShoesTemp2,
+                ShoesHumi2);
+  M5.Lcd.printf("RoomTemp: %f *C \t ShoesHumi2: %f %% \n", RoomTemp,
+                ShoesHumi2);
+  count++;
+  if(count>=500)
+  {
+    analogWrite(FAN, 128);  
+  }   
 }
