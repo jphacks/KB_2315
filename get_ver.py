0dbf16fb8aed3104391adc8a98996dfe2da1c0a5
motty <motty.mio2@gmail.com>
2023-11-11 12:59:42 +0900

    delete unused file

--- get_ver.py
+++
@@ -1,9 +0,0 @@
-import tomllib
-
-from pathlib import Path
-
-with open(Path(__file__).parent / "pyproject.toml", mode="rb") as f:
-    pyproject = tomllib.load(f)
-
-
-print(pyproject["project"]["version"])
