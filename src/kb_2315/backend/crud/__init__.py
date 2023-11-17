d8a03044016460077ed819889470ee7600792ba2
motty <motty.mio2@gmail.com>
2023-11-17 17:05:19 +0000

    channel id を DB から取得するように変更しました

--- src/kb_2315/backend/crud/__init__.py
+++ src/kb_2315/backend/crud/__init__.py
@@ -1,6 +1,7 @@
 from .crud_sensor import crud_sensor
 from .crud_session import crud_session
 from .crud_shoe import crud_shoe
+from .crud_user import crud_user
 
 
-__all__: list[str] = ["crud_sensor", "crud_session", "crud_shoe"]
+__all__: list[str] = ["crud_sensor", "crud_session", "crud_shoe", "crud_user"]
--- src/kb_2315/notify/line.py
+++ src/kb_2315/notify/line.py
@@ -10,14 +10,16 @@ from linebot.models import (
     TextSendMessage,
 )
 
-from kb_2315.backend.crud import crud_shoe
+from kb_2315.backend.crud import crud_shoe, crud_user
 from kb_2315.backend.models import Shoe
 from kb_2315.config import conf
+
+line_channel_id: str = crud_user.search_user_by()[0].line_channel_id
 
 
 def send_message(
     message: str,
-    send_to_id: str = conf.line_group_id,
+    send_to_id: str = line_channel_id,
 ) -> None:
     line_bot_api = LineBotApi(conf.line_channel_access_token)
 
@@ -30,7 +32,7 @@ def send_message(
         print(f"Send Message Error:\n{e}")
 
 
-def shoe_list_carousel(send_to_id: str = conf.line_group_id) -> None:
+def shoe_list_carousel(send_to_id: str = line_channel_id) -> None:
     columns_list: list[CarouselColumn] = []
     shoes: list[Shoe] = crud_shoe.search_shoe_by()
 
@@ -59,7 +61,7 @@ def shoe_list_carousel(send_to_id: str = conf.line_group_id) -> None:
         print(f"LineBotApiError: {e}")
 
 
-def shoe_select_carousel(send_to_id: str = conf.line_group_id, session_id: UUID | None = None) -> None:
+def shoe_select_carousel(send_to_id: str = line_channel_id, session_id: UUID | None = None) -> None:
     columns_list: list[CarouselColumn] = []
     shoes: list[Shoe] = crud_shoe.search_shoe_by()
 
