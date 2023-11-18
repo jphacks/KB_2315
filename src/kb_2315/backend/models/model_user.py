cd42dbd84e34f0c9830defdccca0b26c22d0aafb
motty <motty.mio2@gmail.com>
2023-11-18 14:41:24 +0000

    update db model

--- src/kb_2315/backend/models/model_user.py
+++ src/kb_2315/backend/models/model_user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String
+from sqlalchemy import Float, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from kb_2315.backend.db.base import Base
@@ -8,3 +8,6 @@ class User(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, default="Taro")
     line_channel_id: Mapped[str] = mapped_column(String, nullable=True)
+
+    ido_longitude: Mapped[float] = mapped_column(Float, nullable=False)
+    keido_latitude: Mapped[float] = mapped_column(Float, nullable=False)
